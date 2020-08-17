4ddaabd19f00f6531274b5e8c22b1ba207212559
mhgamble1 <mhgamble1@gmail.com>
2020-08-17 14:33:43 -0500

    updated user model

--- models/users.js
+++ models/users.js
@@ -1,20 +1,5 @@
 const mongoose = require("mongoose");
 
-const ThirdPartyProviderSchema = new mongoose.Schema({
-  provider_name: {
-    type: String,
-    default: null,
-  },
-  provider_id: {
-    type: String,
-    default: null,
-  },
-  provider_data: {
-    type: {},
-    default: null,
-  },
-});
-
 // Create Schema
 const UserSchema = new mongoose.Schema(
   {
@@ -38,23 +23,11 @@ const UserSchema = new mongoose.Schema(
     password: {
       type: String,
     },
-    referral_code: {
-      type: String,
-      default: function () {
-        let hash = 0;
-        for (let i = 0; i < this.email.length; i++) {
-          hash = this.email.charCodeAt(i) + ((hash << 5) - hash);
-        }
-        let res = (hash & 0x00ffffff).toString(16).toUpperCase();
-        return "00000".substring(0, 6 - res.length) + res;
-      },
+    last_login: {
+      type: Date,
+      default: Date.now
     },
-    referred_by: {
-      type: String,
-      default: null,
-    },
-    third_party_auth: [ThirdPartyProviderSchema],
-    date: {
+    joined: {
       type: Date,
       default: Date.now,
     },
